c0db86a937cde02ad3133395c85bb6dd06c0d8c7
Daniel <itzdanielmichael@gmail.com>
2021-11-05 19:21:25 -0400

    update

--- src/com/daniel/minecraftevents/Main.java
+++ src/com/daniel/minecraftevents/Main.java
@@ -13,6 +13,7 @@ import org.bukkit.event.player.PlayerEggThrowEvent;
 // entities
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Egg;
+import org.bukkit.command.*;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -21,6 +22,10 @@ public class Main extends JavaPlugin implements Listener {
 		// when the plugin is ran for first time it will run this block of code.
 
 		System.out.println("EVENTS PLUGIN ENABLED!");
+
+		// referencing the Main class for config.yml
+		this.getConfig().options().copyDefaults(); // getting options and copying the default config here
+		saveDefaultConfig();
 
 		// https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Bukkit.html#getPluginManager()
 		Bukkit.getPluginManager().registerEvents(this, this); // to make events work. args: (listener, plugin)
@@ -41,5 +46,37 @@ public class Main extends JavaPlugin implements Listener {
 		Player player = event.getPlayer();
 		Egg egg = event.getEgg();
 		player.sendMessage(ChatColor.RED + "Egg thrown! " + egg.getEntityId());
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		String cmdName = cmd.getName();
+		Player player = (Player) sender;
+
+		switch (cmdName) {
+		case "config":
+			this.getConfigData(player, true);
+			break;
+
+		case "edit_config":
+			this.editConfigData();
+			break;
+		default:
+			break;
+		}
+		return false;
+	}
+
+	public void getConfigData(Player player, boolean isSendMessage) {
+		String word = this.getConfig().getString("Word");
+		int number = this.getConfig().getInt("Number");
+
+		if (isSendMessage) {
+			player.sendMessage(ChatColor.GRAY + "The word is " + ChatColor.GREEN + word + ChatColor.GRAY
+					+ " And the number " + ChatColor.GREEN + number);
+		}
+	}
+
+	public void editConfigData() {
+		this.getConfig().set("Word", "Apple");
 	}
 }
